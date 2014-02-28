c12e5d1fb93f8afe84c86b9192257f6106ae4b32
rwl <rwl@96a6a1e5-ee6c-4768-9211-b0005cb610e5>
2014-02-28 09:23:25 +0000

    added singlelinkedlist test
    git-svn-id: svn://sun.bitlab.dk/Rasmus/phd/mpccode/parvusfortis@4016 96a6a1e5-ee6c-4768-9211-b0005cb610e5

---
+++ osal/test/singlelinkedlisttest.c
@@ -0,0 +1,38 @@
+#include <osal.h>
+#include <list.h>
+#include <singlelinkedlist.h>
+#include <stdio.h>
+
+#define TEST(EXP,ACT) {                             \
+  if ((EXP) != (ACT)) {                             \
+    printf("Expected %s but got %s\n",(#EXP),(#ACT)); \
+  } else { printf("%s \t [ok]\n", (#ACT)); } }
+
+
+
+
+
+int main(int c, char **a) {
+  OE oe = OperatingEnvironment_LinuxNew();
+  List l = SingleLinkedList_new(oe);
+  int i = 0;
+
+  l->add_element((void*)(ull)42);
+  TEST((void*)(ull)42,l->get_element(0));
+
+  TEST(1,l->size());
+  l->add_element((void*)(ull)43);
+  TEST((void*)(ull)43, l->get_element(1));
+
+
+  for(i = 2;i < 256;++i) {
+    l->add_element((void*)(ull)i);
+    TEST((void*)(ull)i, l->get_element(i));
+  }
+
+  for(i = 2;i < 256;++i) {
+    TEST((void*)(ull)i, l->get_element(i));
+  }
+
+  return 0;
+}
