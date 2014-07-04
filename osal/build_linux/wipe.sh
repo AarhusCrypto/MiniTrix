d94633161376ddb3ae23ce6ebfd72bbd7dd49db9
rwl <rwl@96a6a1e5-ee6c-4768-9211-b0005cb610e5>
2014-07-04 08:27:35 +0000

    git-svn-id: svn://sun.bitlab.dk/Rasmus/phd/mpccode/parvusfortis@4207 96a6a1e5-ee6c-4768-9211-b0005cb610e5

--- osal/build_linux/wipe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rm -rf  aclocal.m4 autom4te.cache/ config.h.in configure src/Makefile.in config.in COPYING missing install-sh INSTALL depcomp config.in headers/Makefile.in
-find . -iname Makefile.in | xargs rm -f
--- osal/headers/singlelinkedlist.h
+++
@@ -1,9 +0,0 @@
-#ifndef SINGLE_LINKED_LIST_H
-#define SINGLE_LINKED_LIST_H
-#include "list.h"
-#include "osal.h"
-
-List SingleLinkedList_new(OE oe);
-void SingleLinkedList_destroy( List * l );
-
-#endif
