f3ddbdee3b10429e45ce137c1675f06343caaf41
Rasmus Zakarias <rwl@cs.au.dk>
2015-08-19 19:14:08 +0200

    updated to the newest subversion version

--- cinterp/circuit/interp.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERP_H
-#define INTERP_H
-#include "ast.h"
-#include <minimacs/minimacs.h>
-
-Visitor mpc_circuit_interpreter(OE oe, AstNode root, MiniMacs mm);
-
-#endif
--- cinterp/circuit/main.c
+++
@@ -1,2 +0,0 @@
-#include "y.tab.h"
-
